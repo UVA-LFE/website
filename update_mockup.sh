ec223cdf5e9b89656d8bcf05e07bfa1e96260d38
Aki Gao <xag9bb@virginia.edu>
2017-05-11 13:18:32 -0400

    actually add completed mock up and shell script to update viewable mock up

---
+++ update_mockup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp /var/folders/6k/pmz0_9l932q453wh0zxxy7ph0000gn/T/Desktop.webp mockup/Desktop.webp
+cp /var/folders/6k/pmz0_9l932q453wh0zxxy7ph0000gn/T/Desktop.html mockup/mockup.html
